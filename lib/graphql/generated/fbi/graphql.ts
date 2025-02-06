55e27f0f3010efd36a2ef1f0385b4c398b2542a9
Jacob Pihl <jacob@youtech.dk>
2025-02-06 13:54:18 +0100

    Run `codegen:graphql`

--- lib/graphql/generated/fbi/graphql.ts
+++ lib/graphql/generated/fbi/graphql.ts
@@ -1654,6 +1654,8 @@ export type Series = {
   alternativeTitles: Array<Scalars['String']['output']>;
   /** Description of the series */
   description?: Maybe<Scalars['String']['output']>;
+  /** The number of members in the series */
+  hitcount: Scalars['Int']['output'];
   /** Additional information  */
   identifyingAddition?: Maybe<Scalars['String']['output']>;
   /** Whether this is a popular series or general series */
@@ -2500,3 +2502,32 @@ useSuspenseGetMaterialQuery.getKey = (variables: GetMaterialQueryVariables) => [
 
 
 useGetMaterialQuery.fetcher = (variables: GetMaterialQueryVariables, options?: RequestInit['headers']) => fetchData<GetMaterialQuery, GetMaterialQueryVariables>(GetMaterialDocument, variables, options);
+
+export const operationNames = {
+  Query: {
+    searchWithPagination: 'searchWithPagination' as const,
+    searchFacets: 'searchFacets' as const,
+    getMaterial: 'getMaterial' as const
+  },
+  Fragment: {
+    SearchFacet: 'SearchFacet' as const,
+    ManifestationCover: 'ManifestationCover' as const,
+    ManifestationIdentifiers: 'ManifestationIdentifiers' as const,
+    ManifestationAccess: 'ManifestationAccess' as const,
+    ManifestationTitles: 'ManifestationTitles' as const,
+    ManifestationLanguages: 'ManifestationLanguages' as const,
+    ManifestationDescription: 'ManifestationDescription' as const,
+    ManifestationDetails: 'ManifestationDetails' as const,
+    ManifestationMaterialTypes: 'ManifestationMaterialTypes' as const,
+    ManifestationSearchPageTeaser: 'ManifestationSearchPageTeaser' as const,
+    ManifestationWorkPage: 'ManifestationWorkPage' as const,
+    WorkAccess: 'WorkAccess' as const,
+    WorkMaterialTypes: 'WorkMaterialTypes' as const,
+    WorkTitles: 'WorkTitles' as const,
+    WorkCreators: 'WorkCreators' as const,
+    WorkPublicationYear: 'WorkPublicationYear' as const,
+    WorkDescription: 'WorkDescription' as const,
+    WorkTeaserSearchPage: 'WorkTeaserSearchPage' as const,
+    WorkFullWorkPage: 'WorkFullWorkPage' as const
+  }
+}
