9177bb247a9dcea0c734b0911553cd8e107fc463
Adam Antal <adoantal@gmail.com>
2025-02-07 14:37:55 +0100

    Add operationNames object to graphql CMS

--- codegen.ts
+++ codegen.ts
@@ -18,7 +18,12 @@ const config: CodegenConfig = {
           },
         },
       },
-      plugins: ["typescript", "typescript-operations", "typescript-react-query"],
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-query",
+        "named-operations-object",
+      ],
       config: {
         enumsAsTypes: true,
         withHooks: true,
@@ -32,6 +37,7 @@ const config: CodegenConfig = {
           transformUnderscore: true,
         },
         fetcher: "@/lib/graphql/fetchers/dpl-cms.fetcher#fetcher",
+        identifierName: "operationNames",
       },
       hooks: {
         // Correcting the codegen output.
